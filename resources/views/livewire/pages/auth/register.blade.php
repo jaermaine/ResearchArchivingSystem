05cfdc2252972ca02ef7fc940ad652354113cf39
Angelo Fabian <151987627+Fabian-AMXYZ@users.noreply.github.com>
2024-10-31 13:21:34 +0800

    Revert "Password requirements (Test)"

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -23,10 +23,7 @@ new #[Layout('layouts.guest')] class extends Component
         $validated = $this->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'string', 'confirmed', Rules\Password::min(8)
-                                                                            ->mixedCase(1)
-                                                                            ->symbols(1)
-                                                                            ->numbers(1)],
+            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $validated['password'] = Hash::make($validated['password']);
