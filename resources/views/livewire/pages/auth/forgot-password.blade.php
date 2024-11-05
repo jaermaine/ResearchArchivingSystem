9140386e612cee198de2831f53247984471cd516
oxsu <romuelborja09@gmail.com>
2024-11-05 20:25:10 +0800

    bg image and coecsa logo png

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -37,7 +37,7 @@ new #[Layout('layouts.guest')] class extends Component
 }; ?>
 
 <div>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+    <div class="mb-4 text-sm text-[#800000] dark:text-[#800000]">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
@@ -47,13 +47,13 @@ new #[Layout('layouts.guest')] class extends Component
     <form wire:submit="sendPasswordResetLink">
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus />
+            <x-input-label for="email" :value="__('Email')" class="text-[#800000] dark:text-[#800000]" />
+            <x-text-input wire:model="email" id="email" class="block mt-1 w-full bg-[#d9d9d9]/0 border-[#800000] rounded-lg dark:bg-[#d9d9d9]/0 dark:border-[#800000]" type="email" name="email" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button class="bg-[#b30000] border-[#800000] text-white dark:bg-[#b30000] dark:border-[#800000]">
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
