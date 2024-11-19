c06e28616ce1ea36aede94b8092224fe7858bf44
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-11-19 09:33:33 +0800

    fix migrations for all tables

---
+++ database/migrations/2024_11_19_000004_create_documents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('field_topic');
+            $table->foreignId('document_status_id')->constrained('document_statuses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
