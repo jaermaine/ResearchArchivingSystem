ee55e2c305605dab80f7e14e5f7e9fea1ed7df3d
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-11-19 22:38:49 +0800

    initial preparations

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,16 @@
-<?php
+@extends('layouts.app')
+
 @section('title', 'Dashboard')
 
+@section('content')
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+
+    <!-- Your dashboard content goes here -->
+
+</x-app-layout>
+@endsection
