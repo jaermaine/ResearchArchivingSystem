beb2d1d438ccd9d8df9d82711acc94655fb5d609
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-11-20 12:38:35 +0800

    ensure role value is inserted

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -41,6 +41,7 @@ new #[Layout('layouts.guest')] class extends Component
         $user = User::create([
             'email' => $validated['email'],
             'password' => $validated['password'],
+            'role' => $validated['role'],
         ]);
 
         if ($validated['role'] === 'student') {
@@ -51,7 +52,7 @@ new #[Layout('layouts.guest')] class extends Component
                 'document_id' => null,
                 'department_id' => null,
             ]);
-        } else {
+        } elseif ($validated['role'] === 'faculty') {
             Faculty::create([
                 'first_name' => $validated['first_name'],
                 'last_name' => $validated['last_name'],
@@ -65,7 +66,14 @@ new #[Layout('layouts.guest')] class extends Component
 
         Auth::login($user);
 
-        $this->redirect(route('dashboard', absolute: false), navigate: true);
+        $redirectRoute = match($validated['role']) {
+            'student' => 'student-dashboard',
+            'faculty' => 'faculty-dashboard',
+            default => 'welcome'
+        };
+        
+        $this->redirect(route($redirectRoute), navigate: true);
+        
     }
 }; ?>
 
