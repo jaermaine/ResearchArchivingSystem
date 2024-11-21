4dca783285c96bde834b69d331d8fd91a4de1382
Fabian-AMXY <fbn.amx@gmail.com>
2024-11-21 23:40:38 +0800

    Small fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,19 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth; // Import Auth facade
-use App\Models\User; // Import the User model
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $user = Auth::user(); // Get the authenticated user
+        $user = User::find(Auth::user()->id); // Get the authenticated user
 
         // Check the user's role and return the appropriate view
-        if ($user->hasRole('faculty')) {
+        if ($user -> hasRole ('faculty')) {
             return view('faculty-dashboard'); // Admin-specific dashboard
-        } elseif ($user->hasRole('student')) {
+        } elseif ($user -> hasRole('student')) {
             return view('student-dashboard'); // Student-specific dashboard
         }
 
