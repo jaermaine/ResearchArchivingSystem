cf70b90bec4f0fe4d3d0b1c9df571fce6b0e452f
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-11-25 10:40:50 +0800

    prepared dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,6 @@
-@extends('layouts.app')
-
 @section('title', 'Dashboard')
 
 @section('content')
-    <div>
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </div>
-
-    <!-- Your dashboard content goes here -->
 
     <!-- Student/Faculty layout content -->
     @if(Auth::user()->role == 'student')
