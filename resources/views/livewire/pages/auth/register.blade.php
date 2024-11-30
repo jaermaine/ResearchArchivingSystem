21a832bb32285bcff12b9778eb6d6676b92c5df8
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-11-30 11:15:58 +0800

    register page removed department selection

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -3,6 +3,7 @@
 use App\Models\User;
 use App\Models\Student;
 use App\Models\Faculty;
+use App\Models\Department;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
@@ -18,12 +19,14 @@ new #[Layout('layouts.guest')] class extends Component
     public string $role = 'student'; // Default role
     public string $first_name = '';
     public string $last_name = '';
+    //public string $department_id = '1';
 
     /**
      * Handle an incoming registration request.
      */
     public function register(): void
     {
+
         $validated = $this->validate([
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
@@ -48,16 +51,14 @@ new #[Layout('layouts.guest')] class extends Component
                 'first_name' => $validated['first_name'],
                 'last_name' => $validated['last_name'],
                 'user_id' => $user->id,
-                'document_id' => null,
-                'department_id' => null,
+                'department_id' => 1,
             ]);
         } elseif ($validated['role'] === 'faculty') {
             Faculty::create([
                 'first_name' => $validated['first_name'],
                 'last_name' => $validated['last_name'],
                 'user_id' => $user->id,
-                'document_id' => null,
-                'department_id' => null,
+                'department_id' => 1,
             ]);
         }
 
@@ -73,14 +74,14 @@ new #[Layout('layouts.guest')] class extends Component
     <form wire:submit="register">
         @csrf
         <!-- First Name -->
-        <div class = "mt-4">
+        <div class="mt-4">
             <x-input-label for="first_name" :value="__('First Name')" class="text-red-600" style="color: #b30000;" />
             <x-text-input wire:model="first_name" id="first_name" class="block mt-1 w-full" type="text" name="first_name" required autofocus autocomplete="name" style="color: black; border: 2px solid #b30000; background-color: #ffffff;" />
             <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
         </div>
 
         <!-- Last Name -->
-        <div class = "mt-4">
+        <div class="mt-4">
             <x-input-label for="first_name" :value="__('Last Name')" class="text-red-600" style="color: #b30000;" />
             <x-text-input wire:model="last_name" id="last_name" class="block mt-1 w-full" type="text" name="last_name" required autofocus autocomplete="last_name" style="color: black; border: 2px solid #b30000; background-color: #ffffff;" />
             <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
@@ -107,6 +108,11 @@ new #[Layout('layouts.guest')] class extends Component
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Department -->
+        <div class = "mt-4">
+            <!-- omit comment once finished livewire('department-list') -->
+        </div> 
+
         <!-- Role -->
         <div class="mt-4">
             <x-input-label for="role" :value="__('Role')" class="text-red-600" style="color: #b30000;" />
@@ -116,6 +122,7 @@ new #[Layout('layouts.guest')] class extends Component
             </select>
             <x-input-error :messages="$errors->get('role')" class="mt-2" />
         </div>
+
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-red-300 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
