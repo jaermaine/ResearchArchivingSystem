fccba7b1240eaea8e09d85bbb2721a79e2991ee6
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-12-04 16:34:16 +0800

    function for document_information to be displayed

--- app/Http/Controllers/SearchDocumentsController.php
+++ app/Http/Controllers/SearchDocumentsController.php
@@ -17,11 +17,22 @@ class SearchDocumentsController extends Controller
             ->where('title', 'like', '%' . $search_input . '%')
             ->orWhere('abstract', 'like', '%' . $search_input . '%')
             ->orWhere('field_topic', 'like', '%' . $search_input . '%')
-            ->select('documents.title', 'documents.abstract', 'documents.field_topic', 'student.first_name', 'student.last_name')
+            ->select('documents.id', 'documents.title', 'student.first_name', 'student.last_name')
             ->get();
 
         $hasResults = count($searchResults);
 
         return view('layouts.search-results', compact('searchResults', 'hasResults'));
+    }
+
+    public function document_info($id){
+        $documentResults = DB::table('documents')
+        ->join('document_student', 'documents.id', '=', 'document_student.document_id')
+        ->join('student', 'document_student.student_id', '=', 'student.id')
+        ->where('documents.id', '=', $id)
+        ->select('documents.id', 'documents.title', 'documents.abstract', 'documents.field_topic', 'student.first_name', 'student.last_name', 'documents.created_at')
+        ->first();
+        
+        return view('layouts.search-results', compact('documentResults'));
     }
 }
