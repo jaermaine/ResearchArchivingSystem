28f205c3ec2db720e14f858792d8c9a0562a3a09
oxsu <romuelborja09@gmail.com>
2025-01-08 22:18:03 +0800

    changed it to be responsive design for desktop and mobile version, also removed the "type = date" as it causes error

--- resources/views/livewire/search-field.blade.php
+++ resources/views/livewire/search-field.blade.php
@@ -1,23 +1,30 @@
-<div class="mt-4 w-full flex items-center">
-    <div class="mr-4">
-        <select wire:model="selectedCategory" class="bg-[#FFC1C1] border-[#808080] outline-none focus:outline-none focus:ring-0 w-[130px] h-[55px]  px-4">
+<div class="mt-4 w-full flex flex-col md:flex-row items-center md:space-x-4 space-y-4 md:space-y-0">
+    <form wire:submit.prevent="search" class="flex w-full flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
+        
+        <!-- Category Dropdown -->
+        <select wire:model="selectedCategory" 
+                class="bg-[#FFC1C1] border-[#808080] outline-none focus:ring-0 w-full md:w-[170px] h-[50px] px-4 rounded-lg">
             <option value="">Any Field</option>
             <option value="title">By Title</option>
             <option value="author">By Author</option>
-            <option value="department">By Department</option>
-            <option value="date">By Date</option>
+            <option value="department">By Dept</option>
+            <option value="date">By Year</option>
         </select>
-    </div>
 
-    <div class="flex items-center bg-[#FFC1C1] rounded-full h-[55px] w-full max-w-[800px] border-2 border-[#808080]">
-    @if($selectedCategory === 'date')
-        <input wire:model="searchInput" wire:keydown.enter="search" type="date" placeholder="Search..." class="bg-transparent border-none outline-none focus:outline-none focus:ring-0 placeholder-gray-500 flex-grow rounded-full h-full px-4" />
-        @else
-        <input wire:model="searchInput" wire:keydown.enter="search" type="text" placeholder="Search..." class="bg-transparent border-none outline-none focus:outline-none focus:ring-0 placeholder-gray-500 flex-grow rounded-full h-full px-4" />
-        @endif
+        <!-- Search Bar -->
+        <div class="flex items-center bg-[#FFC1C1] rounded-full h-[50px] w-full border-2 border-[#808080]">
+            @if($selectedCategory === 'date')
+                <input wire:model="searchInput"
+                       class="bg-transparent border-none focus:ring-0 focus:outline-none placeholder-gray-500 flex-grow h-full px-4 rounded-full" />
+            @else
+                <input wire:model="searchInput" type="text" placeholder="Search..."
+                       class="bg-transparent border-none focus:ring-0 focus:outline-none placeholder-gray-500 flex-grow h-full px-4 rounded-full" />
+            @endif
 
-        <button wire:click="search" type="button" class="flex-none ml-1">
-            <img src="{{ asset('img/search.png') }}" alt="Search" class="w-[45px] h-[45px] md:w-[45px] md:h-[45px]" />
-        </button>   
-    </div>
+            <!-- Search Button -->
+            <button type="submit" class="flex items-center justify-center p-2">
+                <img src="{{ asset('img/search.png') }}" alt="Search" class="w-[35px] h-[35px]" />
+            </button>
+        </div>
+    </form>
 </div>
