8114e04df8b70ac86833a449a9441f4064616bb6
oxsu <romuelborja09@gmail.com>
2025-02-25 09:45:26 +0800

    added an edit for faculty

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/documents/download/{id}', [DocumentStatusController::class, 'download'])->name('download-document');
     Route::get('/documents/approve/{id}', [DocumentStatusController::class, 'approve'])->name('approve-documents');
     Route::get('/documents/reject/{id}', [DocumentStatusController::class, 'reject'])->name('reject-documents');
+    Route::get('/documents/edit/{id}', [DocumentStatusController::class, 'edit'])->name('edit-documents');
 });
 
 Route::get('/search-document/{search_input?}', [SearchDocumentsController::class, 'search_document'])->name('search-document');
