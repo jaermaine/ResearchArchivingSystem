4e910f686f1240fa4c115c875ea8833e9cb9cca2
jaermaine <145512922+jaermaine@users.noreply.github.com>
2025-03-01 22:16:06 +0800

    Update 2025_02_25_065107_add_suffix_to_adviser_and_student.php

--- database/migrations/2025_02_25_065107_add_suffix_to_faculty_and_student.php
+++ database/migrations/2025_02_25_065107_add_suffix_to_faculty_and_student.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up()
     {
-        Schema::table('faculty', function (Blueprint $table) {
+        Schema::table('adviser', function (Blueprint $table) {
             $table->string('suffix')->nullable()->after('last_name');
         });
 
@@ -18,7 +18,7 @@ return new class extends Migration {
 
     public function down()
     {
-        Schema::table('faculty', function (Blueprint $table) {
+        Schema::table('adviser', function (Blueprint $table) {
             $table->dropColumn('suffix');
         });
 
