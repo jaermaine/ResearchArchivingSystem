93b994151ad338b8fb203eb29a206c6c62eafd46
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2025-03-01 12:03:50 +0800

    add Adviser model to handle adviser-related data and relationships

---
+++ app/Models/Adviser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Adviser extends Model
+{
+
+    use HasFactory;
+
+    protected $table = 'adviser';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'user_id',
+        'document_id',
+        'college_id',
+    ];
+
+    public function college(): belongsTo
+    {
+        return $this->belongsTo(College::class);
+    }
+
+    public function users(): belongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function document_adviser(): hasMany
+    {
+        return $this->hasMany(DocumentAdviser::class);
+    }
+
+}
