386841756d0677d516a1f73fd5f97c788e918d99
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2025-03-01 12:03:21 +0800

    update DashboardController to redirect advisers to the adviser dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,8 +12,8 @@ class DashboardController extends Controller
         $user = User::find(Auth::user()->id); // Get the authenticated user
 
         // Check the user's role and return the appropriate view
-        if ($user -> hasRole ('faculty')) {
-            return view('faculty-dashboard'); // Admin-specific dashboard
+        if ($user -> hasRole ('adviser')) {
+            return view('adviser-dashboard'); // Admin-specific dashboard
         } elseif ($user -> hasRole('student')) {
             return view('student-dashboard'); // Student-specific dashboard
         }
