cb5470354c9f8c0a92bfb0eb011cd8a23b06c05c
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2025-03-01 12:06:03 +0800

    add seeders for Adviser, College, DocumentAdviser, and update Document and Student seeders

---
+++ database/seeders/AllSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AllSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+            CollegeSeeder::class,
+            DocumentStatusSeeder::class,
+            DocumentSeeder::class,
+            UserSeeder::class,
+            StudentSeeder::class,
+            AdviserSeeder::class,
+            DocumentAdviserSeeder::class,
+            DocumentStudentSeeder::class,
+        ]);
+    }
+}
