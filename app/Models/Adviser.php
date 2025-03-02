03cd537bf798489f1afefc5fce76691b9645c674
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2025-03-02 16:52:05 +0800

    remove Faculty model and ensure Adviser model references the correct table

--- app/Models/Adviser.php
+++ app/Models/Adviser.php
@@ -6,12 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-
-class Faculty extends Model
-{
-    use HasFactory;
-
-    protected $table = 'adviser'; // Ensure this is 'faculty' and not 'faculties'
 class Adviser extends Model
 {
 
