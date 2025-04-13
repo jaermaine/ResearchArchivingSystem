aef819c85c0a48d5ca55969806c14c9d90190376
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2025-04-13 23:07:41 +0800

    Fix indentation for CSRF token in search field form

--- resources/views/livewire/search-field.blade.php
+++ resources/views/livewire/search-field.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-4 w-full flex flex-col md:flex-row items-center md:space-x-4 space-y-4 md:space-y-0">
     <form wire:submit.prevent="search" class="flex w-full flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
-        
+        @csrf
         <!-- Category Dropdown -->
         <select wire:model="selectedCategory" 
                 class="border-[#808080] outline-none focus:ring-0 w-full md:w-[170px] h-[45px] sm:h-[50px] px-4 rounded-lg text-sm sm:text-base">
